68d8e2bc5b062fe63d62ffa1b2037f60a760bd28
allen <sdfsds5271463@gmail.com>
2026-02-24 15:31:48 +0800

    test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,7 +15,7 @@ import HelloWorld from './components/HelloWorld.vue';
     //const response = await fetch("http://localhost:8080/api/hello");
     const response = await fetch("/api/hello");
     const data = await response.text();
-    console.log(data);
+    console.log(data); //test
   },
 })
 export default class App extends Vue {}
